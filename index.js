f8ad1180413b6e77e65184e777937b50641ccf32
Chayan8837 <145531182+Chayan8837@users.noreply.github.com>
2023-12-21 21:14:15 +0530

    js

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-// let button= document.querySelector(".nav-icon");
-// let right = document.querySelector(".right");
-// let body=document.querySelector("main");
+let button= document.querySelector(".nav-icon");
+let right = document.querySelector(".right");
+let body=document.querySelector("main");
 
-// let  a = right.classList;
-// button.addEventListener(`click`,
-// ()=>
-// {
-//     right.classList.add("show");
-//     button.classList.add("rotate");
-
-
-// })
-// body.addEventListener(`click`,
-// ()=>
-// {
-//     right.classList.remove("show")
-//     button.classList.remove("rotate")
+let  a = right.classList;
+button.addEventListener(`click`,
+()=>
+{
+    right.classList.add("show");
+    button.classList.add("rotate");
 
 
+})
+body.addEventListener(`click`,
+()=>
+{
+    right.classList.remove("show")
+    button.classList.remove("rotate")
 
 
-// })
+
+
+})
 
 
 
