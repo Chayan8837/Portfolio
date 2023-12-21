c25670b2137d67f913a4a088989d984c03cf85c7
Chayan8837 <145531182+Chayan8837@users.noreply.github.com>
2023-12-21 21:11:41 +0530

    popup
    popup

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-let button= document.querySelector(".nav-icon");
-let right = document.querySelector(".right");
-let body=document.querySelector("main");
+// let button= document.querySelector(".nav-icon");
+// let right = document.querySelector(".right");
+// let body=document.querySelector("main");
 
-let  a = right.classList;
-button.addEventListener(`click`,
-()=>
-{
-    right.classList.add("show");
-    button.classList.add("rotate");
+// let  a = right.classList;
+// button.addEventListener(`click`,
+// ()=>
+// {
+//     right.classList.add("show");
+//     button.classList.add("rotate");
 
 
-})
-body.addEventListener(`click`,
-()=>
-{
-    right.classList.remove("show")
-    button.classList.remove("rotate")
+// })
+// body.addEventListener(`click`,
+// ()=>
+// {
+//     right.classList.remove("show")
+//     button.classList.remove("rotate")
 
-})
+
+
+
+// })
+
+
+
+
+let submitbtn=document.querySelector(".popup-submit");
+let cancelbtn=document.querySelector(".cancel");
+let hibox=document.querySelector(".hibox");
+
+submitbtn.addEventListener(`click`,()=>
+ {
+    
+    document.querySelector(".popup").classList.add("hide");
+    
+    
+
+});
+cancelbtn.addEventListener(`click`,()=>
+ {
+    document.querySelector(".popup").setAttribute("class","hide");
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
